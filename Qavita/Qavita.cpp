cffb4c2e4394fce10e3d37cb91fca2350b1a8cb7
AnthonyK213 <tjjzqwh@163.com>
2023-08-11 12:50:39 +0800

    feat: restart kavita

--- Qavita/Qavita.cpp
+++ Qavita/Qavita.cpp
@@ -8,21 +8,47 @@ Qavita::Qavita(QWidget* parent)
     QString icon_path = qApp->applicationDirPath() + "/icons/icon.png";
     m_sysTrayIcon->setIcon(QIcon(icon_path));
     m_sysTrayIcon->setToolTip(QString("Qavita"));
-    m_quitAction = new QAction(QString("Quit"), this);
+
+    m_restartAction = new QAction(tr("Restart"), this);
+    connect(m_restartAction, &QAction::triggered, this, &Qavita::restart);
+
+    m_quitAction = new QAction(tr("Quit"), this);
     connect(m_quitAction, &QAction::triggered, this, &Qavita::on_Quit);
+
     m_menu = new QMenu(QApplication::desktopFileName());
+    m_menu->addAction(m_restartAction);
+    m_menu->addSeparator();
     m_menu->addAction(m_quitAction);
+
     m_sysTrayIcon->setContextMenu(m_menu);
     m_sysTrayIcon->show();
-    QString kavita_dir = qApp->applicationDirPath() + "/bin/Kavita/";
+
+    m_kavita_dir = qApp->applicationDirPath() + "/bin/Kavita/";
     m_kavita = new QProcess();
-    m_kavita->setWorkingDirectory(kavita_dir);
-    m_kavita->start(kavita_dir + "Kavita");
+    m_kavita->setWorkingDirectory(m_kavita_dir);
+
+    start();
+}
+
+void Qavita::start() {
+    m_kavita->start(m_kavita_dir + "Kavita");
+}
+
+void Qavita::close() {
+    m_kavita->terminate();
+}
+
+void Qavita::restart() {
+    if (m_kavita->state() == QProcess::Running) {
+        m_kavita->kill();
+        m_kavita->waitForFinished();
+    }
+    start();
 }
 
 void Qavita::on_Quit() {
     if (nullptr != m_kavita) {
-        m_kavita->terminate();
+        close();
         delete m_kavita;
     }
     qApp->quit();
--- Qavita/Qavita.h
+++ Qavita/Qavita.h
@@ -7,19 +7,28 @@
 
 class Qavita : public QMainWindow
 {
-	Q_OBJECT
+    Q_OBJECT
 
 public:
-	Qavita(QWidget* parent = nullptr);
-	~Qavita();
+    Qavita(QWidget* parent = nullptr);
+    ~Qavita();
 
- private slots:
-	void on_Quit();
+private slots:
+    void start();
+    void close();
+    void restart();
+    void on_Quit();
 
 private:
-	Ui::QavitaClass ui;
-	QSystemTrayIcon* m_sysTrayIcon;
-	QProcess* m_kavita;
-	QAction* m_quitAction;
-	QMenu* m_menu;
+    Ui::QavitaClass ui;
+
+    QSystemTrayIcon* m_sysTrayIcon;
+
+    QMenu* m_menu;
+
+    QAction* m_restartAction;
+    QAction* m_quitAction;
+
+    QProcess* m_kavita;
+    QString m_kavita_dir;
 };
